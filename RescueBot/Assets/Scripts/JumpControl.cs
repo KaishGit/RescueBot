a83e808050f1b0a9719f3faeea9d7d83b2db8ad4
KaishGit <42705648+KaishGit@users.noreply.github.com>
2018-09-30 19:11:30 -0300

    Initial Project

---
+++ RescueBot/Assets/Scripts/JumpControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class JumpControl : MonoBehaviour
+{
+    [SerializeField] float Force;
+
+
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidBody.AddForce(Vector2.up * Force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _rigidBody.AddForce(Vector2.left * Force / 2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _rigidBody.AddForce(Vector2.right * Force / 2);
+        }
+    }
+}
